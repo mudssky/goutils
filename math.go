cf933776f96352ee3e4eae5305ea4384748c254e
mudssky <mudssky@gmail.com>
2023-10-12 10:25:31 +0800

    📃 docs: Deprecated Max

--- math.go
+++ math.go
@@ -14,6 +14,8 @@ func Abs[T c.Number](num T) (res T) {
 
 // MaxN 泛型，返回多个数最大值
 // 暂不对参数数量不符合的清情况做特殊处理
+//
+// Deprecated: go 1.21版本后，官方已经内置了min和max函数，弃用
 func MaxN[T c.Number](first T, others ...T) (res T) {
 	res = first
 	for _, v := range others {
@@ -25,6 +27,8 @@ func MaxN[T c.Number](first T, others ...T) (res T) {
 }
 
 // Max 泛型，返回两个数中最大值
+//
+// Deprecated: go 1.21版本后，官方已经内置了min和max函数，弃用
 func Max[T c.Number](num1 T, num2 T) (res T) {
 	return MaxN(num1, num2)
 }
